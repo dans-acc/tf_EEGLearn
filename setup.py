01c8210fe241d15e2dd7531224eddb5b2143b7fb
dans-acc <danv123456789@gmail.com>
2020-07-26 20:14:35 +0100

    Added a setup message

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='tf_EEGLearn',
+    version='1.11',
+    packages=['EEGLearn'],
+    url='https://github.com/YangWangsky/tf_EEGLearn',
+    license='MIT License',
+    author='YangWangsky',
+    description='EEGLearn implemented using TensorFlow'
+)
